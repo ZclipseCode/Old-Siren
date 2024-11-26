b1b5b0dde17d74e1e7a5fa2652a675dd91d0b597
ZclipseCode <bmichaelbusiness@gmail.com>
2024-11-25 18:22:01 -0600

    display top genres

--- siren-vite/js/music-taste-script.js
+++ siren-vite/js/music-taste-script.js
@@ -1,5 +1,6 @@
 var topArtists = JSON.parse(sessionStorage.getItem("topArtists"));
 var genreOccurences = new Map();
+var sortedGenres;
 
 function setGenreOccurences() {
     for (let i = 0; i < topArtists.length; i++) {
@@ -17,16 +18,30 @@ function setGenreOccurences() {
 }
 
 function sortGenres(a, b) {
-    if (a[1] < b[1]) {
+    if (a.occurences < b.occurences) {
         return -1;
     }
-    if (a[1] > b[1]) {
+    if (a.occurences > b.occurences) {
         return 1;
     }
     return 0;
 }
 
+function setSortedGenres() {
+    let genres = Array.from(genreOccurences, ([genre, occurences]) => ({ genre, occurences }));
+    sortedGenres = genres.sort(sortGenres);
+    sortedGenres.reverse();
+}
+
+function setGenresElement(id) {
+    let genres = document.getElementById(id);
+    genres.textContent = `${sortedGenres[0].genre}, ${sortedGenres[1].genre}, ${sortedGenres[2].genre}... and more!`;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 $(document).ready(function() {
     setGenreOccurences();
-    console.log(genreOccurences);
+    setSortedGenres();
+    setGenresElement('genres');
 });
